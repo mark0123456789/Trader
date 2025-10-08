b2a1262632f442936f3c2a02542d1daefbce8f56
piringerm <piringerm@301-15>
2025-10-08 08:21:11 +0200

    a

---
+++ Trader/connect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Trader
+{
+    internal class connect
+    {
+        public MySqlConnection _Connection;
+
+        private String _host;
+        private String _db;
+        private String _user;
+        private String _password;
+
+        private String _Connectionstring;
+
+        public Connect() {
+         _host = "localhost";
+        _host = "trader";
+            _user = "root";
+            _password = "";
+        
+            _Connectionstring = $"SERVER={_host};DATABASE={_db};UID={_user},PASSWORD={_password};SslMode=None";
+
+            _Connection = new MySqlConnection(_host);
+        }
+    }
+}
